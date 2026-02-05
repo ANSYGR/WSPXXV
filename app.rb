849b40cae1ef5d616b4d1d54ada5cdae9c380691
andy granfors <andy.granfors@elev.ga.ntig.se>
2026-02-05 16:39:44 +0100

    Påbörjad struktur

--- app.rb
+++ app.rb
@@ -3,3 +3,25 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
+
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+get '/' do
+	@title = 'Home'
+	slim :index
+end
+
+get '/marketplace' do
+	@title = 'Marketplace'
+	slim :marketplace
+end
+
+get '/forum' do
+	@title = 'Forum'
+	slim :forum
+end
+
+get '/shop' do
+	@title = 'Shop'
+	slim :shop
+end
